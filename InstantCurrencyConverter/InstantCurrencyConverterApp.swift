f9468ec32b38dd3bc90e79251ddc8f9db61632d6
Fly <kiwipromm@gmail.com>
2023-03-17 09:11:24 +0100

    Initial Commit

---
+++ InstantCurrencyConverter/InstantCurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstantCurrencyConverterApp.swift
+//  InstantCurrencyConverter
+//
+//  Created by KADIR ISMAILI on 17.3.23.
+//
+
+import SwiftUI
+
+@main
+struct InstantCurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
